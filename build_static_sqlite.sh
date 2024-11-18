dade7feefd0c57172f34cf968c0f9bfff5e43820
Bill Wenrich <bwenrich@users.noreply.github.com>
2024-11-18 10:52:06 -0500

    bump to sqlite 3.47.0

--- build_static_sqlite.sh
+++ build_static_sqlite.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SQLITE_ZIP_URL='https://sqlite.org/2024/sqlite-amalgamation-3460100.zip'
+SQLITE_ZIP_URL='https://sqlite.org/2024/sqlite-amalgamation-3470000.zip'
 SQLite_compressor='upx'  # Program to use for compressing compiled sqlite
                          # Keep it empty as "" to disable compression
 
